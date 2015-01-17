2762f73aa1711dc5741e84d766df9e5f8b093105
Wasif Hasan Baig <pr.wasif@gmail.com>
2015-01-17 11:41:09 -0600

    gulpfile updated

--- gulpfile.js
+++ gulpfile.js
@@ -2,81 +2,46 @@ var gulp = require('gulp');
 var help = require('gulp-help');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var del = require('del');
 
-/** gulp tasks */
+/* gulp CONSTANTS */
+var MIN_JS_NAME = 'jqm.extended.collapsible.min.js';
+var MIN_CSS_NAME = 'jqm.extended.collapsible.min.css';
+var DIST_PATH = 'dist/';
+var TEMP_PATH = 'temp/';
+var CLEAN_PATHS = ['temp/**', 'dist/**'];
+
+/* gulp tasks */
 
 help(gulp);
 
 gulp.task('clean', 'Cleans the build folder.', [], function () {
     "use strict";
     
-    del([
-        'temp/**',
-        'dist/**',
-    ]);
+    del(CLEAN_PATHS);
     
-}, {
-    aliases: ['c', 'C']
-});
+}, { aliases: ['c', 'C'] });
 
 gulp.task("concat", 'Joins all the script files putting them in build folder.', [], function () {
     "use strict";
     gulp.src(['js/**/*.js'])
-        .pipe(concat('jqm.collapsible.concat.js'))
-        .pipe(gulp.dest('temp/'));
-}, {
-    aliases: ['j', 'J']
-});
+        .pipe(concat('jqm.extended.collapsible.js'))
+        .pipe(gulp.dest(DIST_PATH));
+}, { aliases: ['j', 'J'] });
 
 gulp.task("minify", 'Minifies all the script files.', ['concat'], function () {
     "use strict";
-    gulp.src(['temp/*.concat.js'])
+    gulp.src(['dist/*.js'])
         .pipe(uglify({
             preserveComments: 'some'
         }))
-        .pipe(rename('jqm.collapsible.min.js'))
-        .pipe(gulp.dest('dist/'));
-}, {
-    aliases: ['m', 'M']
-});
+        .pipe(rename(MIN_JS_NAME))
+        .pipe(gulp.dest(DIST_PATH));
+}, { aliases: ['m', 'M'] });
 
-gulp.task("min-css", 'Minifies the CSS stylesheets.', [], function() {
-    "use strict";
-    gulp.src('css/**/*.css')
-        .pipe(concat('jqm.collapsible.min.css'))
-        .pipe(minifyCss({
-            noAdvanced: false
-        }))
-        .pipe(gulp.dest('dist/'));
-}, {
-    aliases: ['s', 'S']
-});
-
-gulp.task("assets", 'Copies all assets (css stylesheets, images etc.) to the build folder.', [], function(){
-    "use strict";
-    gulp.src("css/**/*")
-        .pipe(concat("jqm.collapsible.css"))
-        .pipe(gulp.dest(DEST));
-}, {
-    aliases: ['a', 'A']
-});
-
-gulp.task("build", '(default task) Cleans, concatenates and minifies all script files into build folder.', [], function () {
-    "use strict";
-    gulp.run('clean');
-    gulp.run('concat');
-    gulp.run('minify');
-//    gulp.run('assets');
-//    gulp.run('minify-css');
-}, {
-    aliases: ['b', 'B']
-});
-
-gulp.task('default', function () {
-    "use strict";
-    gulp.run('build');
-});
-
+gulp.task(  'default',
+            '(default task) Cleans, concatenates and minifies all script files into build folder.',
+            ['clean', 'concat', 'minify'],
+            function () {},
+            { aliases: ['d', 'D'] } );
